0008550e29e763a24a0989fb71be5e8480a05916
ASUS <ASUS@LAPTOP-40J96RUH>
2022-10-23 19:59:20 +0700

    Add project files.

---
+++ P5_3_1214079/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_1214079
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsbMouse_Click(object sender, EventArgs e)
+        {
+            if (rbMouse.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Mouse";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void tsbMonitor_Click(object sender, EventArgs e)
+        {
+            if (rbMonitor.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Monitor";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void tsbPrinter_Click(object sender, EventArgs e)
+        {
+            if (rbPrinter.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Printer";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void tsbKeyboard_Click(object sender, EventArgs e)
+        {
+            if (rbKeyboard.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Keyboard";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void rbMonitor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbMonitor.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Monitor";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void rbKeyboard_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbKeyboard.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Keyboard";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void rbMouse_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbMouse.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Mouse";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+
+        private void rbPrinter_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbPrinter.Checked == true)
+            {
+                tslKetGambar.Text = "Cocok ini adalah gambar Printer";
+            }
+            else
+            {
+                tslKetGambar.Text = "Tidak cocok! Anda memilih gambar yang salah";
+            }
+        }
+    }
+}
